8cd9da86e592eed3cab961de3bcdb1066b3b0d76
K Kiran Kumar <113544336+kiran150502@users.noreply.github.com>
2022-10-17 14:51:15 +0530

    Add files via upload

---
+++ string_types.py
@@ -0,0 +1,24 @@
+
+print(type('hello'))
+print(type('pavan'))
+print('hello\n'*3,end='')
+
+print('hello'*5)
+print('pavan'*2)
+
+print('hello''/'*9)
+print('pavan'+'/'*3)
+
+print('hello'+''+'/')
+print('pavan'+''+'/')
+
+print('hello'+'  '+'  '+'pavan')
+print(1,2,3,4,5,sep='')
+print(9,8,7,6,5,sep=' ')
+
+Today is thursday time is 10:31
+print('Today is thursday time is 10:31')
+print("Today is thursday time is 10:31")
+print('''Today is thursday time is 10:31''')
+print("""Today is thursday time is 10:31""")
+print('Today'+' '+'is'+' '+'thursday'+' '+'time'+' '+'is'+' '+'10:31')
